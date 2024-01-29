15c4c478fe4d331e24890bb5a6635516f5b824ea
ross1456 <99025313+ross1456@users.noreply.github.com>
2024-01-29 16:32:39 +0800

    Add files via upload

---
+++ ATE_package/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+  
+with open("README.md", "r") as fh: 
+    description_long = fh.read() 
+  
+setup( 
+    name="ATE", 
+    version="0.0.1", 
+    author="Ross", 
+    author_email="rosscooper@link.cuhk.edu.hk", 
+    packages=find_packages(),
+    description=" Nonparametric efficient inference of average treatment effects for observational data", 
+    long_description=description_long, 
+    long_description_content_type="text/markdown", 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+        ],
+    url="https://github.com/ross1456/CIBer-Package.git", 
+    license='MIT', 
+    python_requires='>=3.8', 
+    install_requires=["numpy","pandas","scipy","matplotlib","warnings"] 
+)
